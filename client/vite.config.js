dd7ba42b37ccc9d3f21c84d15f148f07111ccf5e
Binada <binada2005@gmail.com>
2025-07-28 02:13:20 +0530

    Your update message here

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,9 +10,8 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api': "http://localhost:3000",
-        
-      }
+      "/api": "http://localhost:3000"
     }
-  },
-)
+  }
+  });
+
