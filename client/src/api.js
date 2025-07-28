f378f63289ff55bce5af7e6697f1e887f191c8d8
Binada <binada2005@gmail.com>
2025-07-29 01:13:25 +0530

    Add feature X and fix bug Y

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,18 @@
-// src/api.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://cricblog-backend.onrender.com/api', // e.g. https://cricblog-backend.onrender.com/api
+  baseURL: 'https://cricblog-backend.onrender.com/api',
+});
+
+// Add a request interceptor to include token in headers
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // or wherever you store your token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
